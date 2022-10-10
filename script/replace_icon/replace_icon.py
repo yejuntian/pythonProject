fa56069d72f8a5ffa3e4d1c2939a9569523c5603
tianyejun <tianyejun@ushareit.com>
2022-10-10 16:52:34 +0800

    [Feature]:优化替换资源配置

--- script/replace_icon/replace_icon.py
+++ script/replace_icon/replace_icon.py
@@ -7,9 +7,7 @@ import xml.etree.ElementTree as ET
 blacklist = ['.idea', '.git', 'build', 'kotlin', 'lib', 'META-INF',
              'original', 'AndroidManifest.xml', 'apktool.yml']
 # 要复制的icon列表
-icon_list = ["share_self.jpg", "about_logo.png", "icon.png",
-             "splash_logo.png", "ic_wa_foreground.png", "img_0.png",
-             "img_1.png", "background.jpg"]
+icon_list = []
 
 """
     主要作用：根据"icon_list"图片资源列表，
@@ -17,7 +15,25 @@ icon_list = ["share_self.jpg", "about_logo.png", "icon.png",
 """
 
 
-def replace_icon(from_path, to_path, black_list):
+def delOldIcon(from_path, black_list):
+    from_listdir = os.listdir(from_path)
+    for file_name in from_listdir:
+        from_file_path = os.path.join(from_path, file_name)
+        if file_name not in black_list and not file_name.__contains__("smali"):
+            if os.path.isdir(from_file_path):
+                # 过滤不需要遍历的文件夹
+                if enable_traver(file_name, "layout") and enable_traver(file_name, "values") and enable_traver(
+                        file_name, "xml"):
+                    # print(f"遍历文件：{file_name}")
+                    delOldIcon(from_file_path, blacklist)
+                else:
+                    continue
+            else:
+                if file_name in icon_list:
+                    os.remove(from_file_path)
+
+
+def copyIcon(from_path, to_path, black_list):
     from_listdir = os.listdir(from_path)
     for file_name in from_listdir:
         from_file_path = os.path.join(from_path, file_name)
@@ -28,13 +44,14 @@ def replace_icon(from_path, to_path, black_list):
                 if enable_traver(file_name, "layout") and enable_traver(file_name, "values") and enable_traver(
                         file_name, "xml"):
                     # print(f"遍历文件：{file_name}")
-                    replace_icon(from_file_path, to_file_path, blacklist)
+                    copyIcon(from_file_path, to_file_path, blacklist)
                 else:
                     continue
             else:
                 if file_name in icon_list:
                     if not os.path.exists(to_path):
                         os.makedirs(to_path, exist_ok=True)
+                    print(file_name)
                     shutil.copyfile(from_file_path, to_file_path)
 
 
@@ -59,5 +76,6 @@ if __name__ == "__main__":
 
     icon_list = load_data("script/replace_icon/res_icon.xml")
 
-    replace_icon(args.from_project_dir, args.to_project_dir, blacklist)
+    # delOldIcon(args.to_project_dir, blacklist)
+    copyIcon(args.from_project_dir, args.to_project_dir, blacklist)
     print(f"执行完成，输出结果保存到：{args.to_project_dir}")
