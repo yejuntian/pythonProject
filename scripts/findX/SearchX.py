30e4305d52bc4b1c73df4d4e1382e8724924a3d5
tianyejun <tianyejun@ushareit.com>
2022-11-20 17:58:48 +0800

    [Feature]:优化脚本

--- scripts/findX/SearchX.py
+++ scripts/findX/SearchX.py
@@ -1,9 +1,16 @@
+import argparse
 import codecs
 import json
 import os
 import re
-import argparse
 
+foldPath = ""
+# 排除哪些文件夹
+blacklist = ['.idea', '.git', 'build', 'assets', 'kotlin', 'lib', 'META-INF',
+             'original', 'res', 'smali', 'smali_classes2', 'smali_classes3',
+             'smali_classes4', 'smali_classes7', 'AndroidManifest.xml', 'apktool.yml']
+# 只匹配下面的文件类型
+extends = ["smali"]
 savePath = ""
 setList = set()
 mapList = {}
@@ -11,27 +18,27 @@ baseVersion = "2.22.18.70"
 newVersion = "2.22.22.80"
 
 
-def findXClass(folder_path, rexStr):
-    os.chdir(folder_path)
-    cwd = os.getcwd()
-    fileList = os.listdir(cwd)
+def findXClass(from_dir, rexStr):
+    fileList = os.listdir(from_dir)
     for fileName in fileList:
-        fpath = os.path.join(cwd, fileName)
-        if os.path.isfile(fpath) and fpath.split('.')[-1] == "smali":
+        fpath = os.path.join(from_dir, fileName)
+        if os.path.isfile(fpath) and fpath.split('.')[-1] in extends:
+            print(fpath)
             with codecs.open(fpath, "r", "utf-8") as rfile:
                 data = rfile.read()
                 findList = re.findall(fr"{rexStr}", data)
+                filePath = fpath.split(f"{foldPath}/")[1]
                 for findStr in findList:
                     if findStr in mapList:
                         value = mapList[findStr]
-                        value.add(fileName)
+                        value.add(filePath)
                     else:
                         classSet = set()
-                        classSet.add(fileName)
+                        classSet.add(filePath)
                         mapList[findStr] = classSet
                 setList.update(findList)
         # 如果是文件夹，递归
-        elif os.path.isdir(fpath):
+        elif os.path.isdir(fpath) and fileName not in blacklist:
             findXClass(fpath, rexStr)
 
 
@@ -74,6 +81,8 @@ if __name__ == "__main__":
                "|HomeActivity;->A\w+\(.*?\).+"
                "|StatusesFragment;->A\w+\(.*?\).+"
                "|Lcom/gbwhatsapp/Conversation;->A\w+\(.*?\).+"
+               "|Landroidy/recyclerview/widget/LinearLayoutManager;->A\w+\(.*?\).+"
+               "|Lcom/gbwhatsapp/RequestPermissionActivity;->A\w+\(.*?\).+"
                )
     save2File(mCurPath, package_data(), "method.json")
     # 查找属性
