6fe608544eab4f0f2b2d9c0b4673156592e297e4
tianyejun <tianyejun@ushareit.com>
2022-11-27 21:22:31 +0800

    [Feature]:添加替换layout TextView脚本

---
+++ scripts/repalce_layout/textview/replace_TextView.py
@@ -0,0 +1,55 @@
+import codecs
+import os
+import xml.etree.ElementTree as ET
+
+oldStr = "<TextView"
+replaceStr = "<com.gbwhatsapp.yo.tf"
+file_list = []
+
+
+def replaceText(from_dir, dataList):
+    listdir = os.listdir(from_dir)
+    for fname in listdir:
+        fpath = os.path.join(from_dir, fname)
+        if os.path.isdir(fpath):
+            replaceText(fpath, dataList)
+        elif os.path.isfile(fpath) and fname.split(".")[-1] == "xml":
+            if fname in dataList:
+                startReplaceXml(fpath, fname)
+
+
+def startReplaceXml(file_path, fileName):
+    with codecs.open(file_path, encoding="utf-8", mode="r") as rf:
+        data = rf.read()
+    with codecs.open(file_path, "w", "utf-8") as wfile:
+        count = data.count(oldStr)
+        if count > 0:
+            file_list.append(fileName)
+            data = data.replace(oldStr, replaceStr)
+        wfile.write(data)
+
+
+def printResult(dataList, dir_path):
+    data = []
+    for fname in dataList:
+        if fname not in file_list:
+            data.append(fname)
+    if len(data) > 0:
+        print(f"没有执行到的文件列表为：{data}")
+    print(f"执行完毕，输出结果保存到：{dir_path}")
+
+
+def loadData(path):
+    parse = ET.parse(path)
+    root = parse.getroot()
+    data_list = []
+    for child in root:
+        data_list.append(child.text)
+    return data_list
+
+
+if __name__ == "__main__":
+    from_dir = "/Users/shareit/work/shareit/wa_diff_gb/whatsapp_2.22.22.80"
+    dataList = loadData("TextView.xml")
+    replaceText(f"{from_dir}/res", dataList)
+    printResult(dataList, f"{from_dir}/res")
