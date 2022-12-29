529d8e8cc0b05ee9980f7d888779edd8c3f888be
tianyejun <tianyejun@ushareit.com>
2022-12-29 17:57:59 +0800

    [Feature]:优化脚本

--- scripts/app_exipire_time.py
+++ scripts/app_exipire_time.py
@@ -22,4 +22,4 @@ print(f'5、WhatsApp登录强升级开始时间:{time.strftime("%Y年%m月%d日 
 currentTime = baseTime + (180 + 7) * 86400
 print(f'6、GB版本内置本地升级时间:{time.strftime("%Y年%m月%d日 %H时:%M分:%S秒", time.localtime(currentTime))}')
 
-print(f'7、GB版本内置本地升级时间对应毫秒：{currentTime * 1000}')
+print(f'7、GB版本内置本地升级时间对应毫秒：{str(float(currentTime * 1000)).split(".")[0]}')
