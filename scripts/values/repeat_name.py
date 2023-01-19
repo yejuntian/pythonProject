3f492effbb021c74b6087102669ec36e06e71a69
tianyejun <tianyejun@ushareit.com>
2023-01-19 15:53:59 +0800

    [Feature]:优化查找重复name的脚本

--- scripts/values/repeat_name.py
+++ scripts/values/repeat_name.py
@@ -1,12 +1,11 @@
 import codecs
 import json
 import os
+import argparse
 import xml.etree.ElementTree as ET
 
-# 用于存放重复的name
-repeatNameList = []
-# 用于存放所有name
-nameList = []
+# 用于存放xml文件名字和重复name属性的对应关系
+repeatNameDict = {}
 
 """
     主要作用：查找string.xml、styles.xml、colors.xml
@@ -14,10 +13,42 @@ nameList = []
 """
 
 
-def findRepeatName(fpath):
+def checkRepeatName(from_dir):
+    transFolder(from_dir)
+    isExit = False
+    for attrType, attrList in repeatNameDict.items():
+        if len(attrList) > 0:
+            isExit = True
+
+    if isExit:
+        save_2_file(repeatNameDict, f"{os.getcwd()}/scripts/values/repeatName.json")
+    else:
+        print(f"*** {from_dir} *** 不存在重复的属性name")
+
+
+def transFolder(from_dir):
+    if os.path.isdir(from_dir):
+        listdir = os.listdir(from_dir)
+        for fname in listdir:
+            fpath = os.path.join(from_dir, fname)
+            if os.path.isdir(fpath):
+                transFolder(from_dir)
+            elif os.path.isfile(fpath) and not fname == "public.xml":
+                findRepeatName(fpath, fname.split(".")[0])
+    else:
+        findRepeatName(from_dir, from_dir.split(".")[0])
+
+
+def findRepeatName(fpath, attrType):
+    if repeatNameDict.get(attrType) is None:
+        repeatNameDict[attrType] = []
+
     parser = ET.parse(fpath)
     root = parser.getroot()
-
+    # 用于存放重复的name
+    repeatNameList = []
+    # 用于存放所有name
+    nameList = []
     for child in root:
         attrib = child.attrib
         name = attrib.get("name")
@@ -27,12 +58,7 @@ def findRepeatName(fpath):
             nameList.append(name)
         else:
             repeatNameList.append(name)
-    fileName = f'{os.path.basename(fpath).split(".")[0]}.json'
-    newPath = os.path.join(os.getcwd(), fileName)
-    if len(repeatNameList) <= 0:
-        print(f"*** {os.path.basename(fpath)} *** 不存在重复的属性name")
-    else:
-        save_2_file(repeatNameList, newPath)
+    repeatNameDict[attrType] = repeatNameList
 
 
 def save_2_file(dataList, fpath):
@@ -43,5 +69,9 @@ def save_2_file(dataList, fpath):
 
 
 if __name__ == "__main__":
-    from_dir = "/Users/shareit/work/GBWorke/whatsapp_new/Whatsapp_v2.22.24.78/res/values-night/colors.xml"
-    findRepeatName(from_dir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from_dir")
+    args = parser.parse_args()
+    from_dir = args.from_dir
+    # from_dir = "/Users/shareit/work/shareit/gbwhatsapp/DecodeCode/Whatsapp_v2.22.24.78"
+    checkRepeatName(f"{from_dir}/res/values")
