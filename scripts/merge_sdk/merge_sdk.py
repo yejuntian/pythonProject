f1c0c39c9ce8db5e2dfc923ec640552d509ad2b9
tianyejun <tianyejun@ushareit.com>
2023-04-13 17:51:37 +0800

    [Feature]:优化脚本

--- scripts/merge_sdk/merge_sdk.py
+++ scripts/merge_sdk/merge_sdk.py
@@ -3,10 +3,13 @@ import glob
 import os.path
 import json
 import codecs
+import re
 import xml.etree.ElementTree as ET
 
 # 用来保存没有发现集合
 notFoundDict = {}
+# 用来保存错误attr集合列表
+attrDict = {}
 """
     主要作用：替换sdk目录R$*.smali资源文件value值
 """
@@ -22,36 +25,87 @@ def merge_sdk(sdk_dir, public_dir):
     public_map = get_public_data(public_dir)
 
     for file_path in file_list:
-        write_data_2_file(file_path, public_map)
+        setAllAttr(file_path, public_map)
+    # R$styleable.smali value值
+    print(attrDict)
+    for file_path in file_list:
+        if os.path.basename(file_path) == "R$styleable.smali":
+            replaceStyleable(file_path, public_map)
+        else:
+            write_data_2_file(file_path, public_map)
     save2File(notFoundDict, f"{os.getcwd()}/scripts/values/native_values/GBNeedToFind.json")
+
+
+# 设置所有属性到attrDict
+def setAllAttr(file_path, public_map):
+    file_name = os.path.basename(file_path)
+    file_type = file_name.split("$")[1].split(".")[0]
+    attrList = public_map.get("attr")
+    with open(file_path, encoding="utf-8", mode="r") as rf:
+        lines = rf.readlines()
+        for line in lines:
+            if line.startswith(".field public static final"):
+                attr_name = line.split(":")[0].split(" ")[-1]
+                attr_value = line.split("=")[-1].strip()
+                newId = attrList.get(attr_name)
+                # 用来保存所有错误的attr属性
+                if file_type == "attr" and newId is not None and newId != attr_value:
+                    # print(f"attr_value = {attr_value} attr_name = {attr_name}")
+                    attrDict[attr_value] = attr_name
+
+
+def replaceStyleable(file_path, public_map):
+    attrIdList = public_map["attr"]
+    with codecs.open(file_path, "r", "utf-8") as rf:
+        lines = rf.readlines()
+    with codecs.open(file_path, "w", "utf-8") as wf:
+        data = getReplaceResult(lines, attrIdList)
+        wf.write(data)
+
+
+def getReplaceResult(lines, attrIdList):
+    regex = r"0x7f\w{6}"
+    result = ""
+    for index, line in enumerate(lines):
+        matches = re.finditer(regex, line, re.MULTILINE)
+        for matchNum, match in enumerate(matches, start=1):
+            attrValueId = match.group()
+            attrName = attrDict.get(attrValueId)
+            # if attrValueId == '0x7f0407ca':
+            #     print("attrName = " + attrName)
+            # print(f"attrValueId = {attrValueId} attrName = {attrName}")
+            if attrName is not None and attrIdList.get(attrName) is not None:
+                line = line.replace(attrValueId, attrIdList.get(attrName))
+        result += line
+    return result
 
 
 def write_data_2_file(file_path, public_map):
     file_name = os.path.basename(file_path)
     file_type = file_name.split("$")[1].split(".")[0]
-    if file_name != "R$styleable.smali":
-        data = ""
-        with open(file_path, encoding="utf-8", mode="r") as rf:
-            lines = rf.readlines()
-            for line in lines:
-                if line.startswith(".field public static final"):
-                    attr_name = line.split(":")[0].split(" ")[-1]
-                    # print(f"file_type = {file_type} attr_name = {attr_name}")
-                    # 用来保存没有找到的属性到notFoundDict
-                    if notFoundDict.get(file_type) is None:
-                        notFoundDict[file_type] = []
-                    if not attr_name in public_map[file_type]:
-                        notFoundDict[file_type].append(attr_name)
-                    # 只有匹配到的才进行赋值
-                    if not public_map[file_type].get(attr_name) is None:
-                        attr_id = public_map[file_type][attr_name]
-                        data += f".field public static final {attr_name}:I = {attr_id}\n"
-                    else:  # 否则保留之前的value数值
-                        data += line
-                else:
+    data = ""
+    with open(file_path, encoding="utf-8", mode="r") as rf:
+        lines = rf.readlines()
+        for line in lines:
+            if line.startswith(".field public static final"):
+                attr_name = line.split(":")[0].split(" ")[-1]
+                attr_value = line.split("=")[-1].strip()
+                # print(f"file_type = {file_type} attr_name = {attr_name}")
+                # 用来保存没有找到的属性到notFoundDict
+                if notFoundDict.get(file_type) is None:
+                    notFoundDict[file_type] = []
+                if not attr_name in public_map[file_type]:
+                    notFoundDict[file_type].append(attr_name)
+                # 只有匹配到的才进行赋值
+                if not public_map[file_type].get(attr_name) is None:
+                    attr_id = public_map[file_type][attr_name]
+                    data += f".field public static final {attr_name}:I = {attr_id}\n"
+                else:  # 否则保留之前的value数值
                     data += line
-        with open(file_path, encoding="utf-8", mode="w") as wf:
-            wf.write(data)
+            else:
+                data += line
+    with open(file_path, encoding="utf-8", mode="w") as wf:
+        wf.write(data)
 
 
 def save2File(dataList, fpath):
