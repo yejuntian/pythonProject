13ac0ad3276928c2005cdbbe3dd44b7d9f494bec
tianyejun <tianyejun@ushareit.com>
2023-07-20 16:09:52 +0800

    [Feature]: 优化重命名资源的脚本

--- scripts/values/replace_layout/replace_layout.py
+++ scripts/values/replace_layout/replace_layout.py
@@ -1,6 +1,7 @@
 import codecs
 import json
 import argparse
+import xml.etree.ElementTree as ET
 import os
 
 # 只匹配下面的文件类型
@@ -10,7 +11,11 @@ blacklist = ['.idea', '.git', 'build', 'assets', 'kotlin',
              'lib', 'META-INF', 'original', 'apktool.yml']
 # 存储对应关系文件
 dataPath = 'scripts/values/replace_layout/layout.json'
-
+# 保存public.xml对应关系
+publicTypeDic = {}
+publicIdDic = {}
+# 用于存放重复的属性名称
+repeatName = {}
 """
     主要作用：根据layout.json中对应关系，
     替换项目所涉及的res/layout目录下的xml布局文件名
@@ -32,10 +37,10 @@ def execute_folder(from_dir, blacklist, extends, mapping_string):
             elif os.path.isfile(fpath):
                 if fname.split(".")[-1] in extends:
                     print(fpath)
-                    save_2_file(fpath, fname, from_dir)
+                    save_2_file(fpath, fname, from_dir, mapping_string)
 
 
-def save_2_file(fpath, fname, from_dir):
+def save_2_file(fpath, fname, from_dir, mapping_string):
     enableRenameFile = False
     with codecs.open(fpath, mode="r", encoding="utf-8") as rf:
         data = rf.read()
@@ -43,18 +48,52 @@ def save_2_file(fpath, fname, from_dir):
         replace_times = 0
         for key, value in mapping_string.items():
             if key.startswith("APKTOOL_DUMMYVAL_"):
-                replace_times += data.count(key)
-                data = data.replace(key, value)
-                if key == fname.split(".")[0]:
-                    enableRenameFile = True
-                    newPath = os.path.join(from_dir, f"{value}.xml")
+                # 重命名之前，判断是否已存在重命名后的属性名称
+                attrType = publicIdDic.get(key)
+                attrNameList = publicTypeDic.get(attrType)
+                # 判断重命名后的属性不存在,再进行重命名操作
+                if attrNameList is None or value not in attrNameList:
+                    replace_times += data.count(key)
+                    data = data.replace(key, value)
+                    if key == fname.split(".")[0]:
+                        enableRenameFile = True
+                        newPath = os.path.join(from_dir, f"{value}.xml")
+                else:
+                    # 存放重复的属性key
+                    repeatName[value] = attrType
         print(r'替换次数：', replace_times)
         wf.write(data)
     if enableRenameFile:
-        # # 解决重复发执行脚本，对重命名的文件进行还原
-        # if os.path.exists(newPath):
-        #     os.rename(newPath, fpath)
         os.rename(fpath, newPath)
+
+
+def main(mCurrentPath, from_dir):
+    mapping_string = load_replace_keys(f"{mCurrentPath}/{dataPath}")
+    parserPublicXml(f"{from_dir}/res/values/public.xml")
+    execute_folder(from_dir, blacklist, extends, mapping_string)
+    if len(repeatName) > 0:
+        print("****************重复的属性名称如下：****************")
+        print(repeatName)
+
+
+# 解析public.xml
+def parserPublicXml(fpath):
+    parser = ET.parse(fpath)
+    root = parser.getroot()
+    for child in root:
+        attrib = child.attrib
+        attrName = attrib.get("name")
+        attrType = attrib.get("type")
+        attrId = attrib.get("id")
+        if attrType is None or attrName is None:
+            continue
+        if publicTypeDic.get(attrType) is None:
+            publicTypeDic[attrType] = []
+        else:
+            publicTypeDic[attrType].append(attrName)
+        # 保存name和type的对应关系
+        if attrName.startswith("APKTOOL_DUMMYVAL_0x7f"):
+            publicIdDic[attrName] = attrType
 
 
 if __name__ == '__main__':
@@ -64,5 +103,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     from_dir = args.from_dir
     # from_dir = "/Users/shareit/work/GBWorke/whatsapp_new/Whatsapp_v2.22.24.78"
-    mapping_string = load_replace_keys(f"{mCurrentPath}/{dataPath}")
-    execute_folder(from_dir, blacklist, extends, mapping_string)
+    main(mCurrentPath, from_dir)
