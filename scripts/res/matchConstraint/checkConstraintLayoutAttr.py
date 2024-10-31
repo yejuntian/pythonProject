39384df89d1b4ecc9c28bfa29b16016296782288
tianyejun <tianyejun@adsconflux.com>
2024-10-31 17:18:00 +0800

    [Feature]: 优化脚本

--- scripts/res/matchConstraint/checkConstraintLayoutAttr.py
+++ scripts/res/matchConstraint/checkConstraintLayoutAttr.py
@@ -1,46 +1,9 @@
-import os
 import xml.etree.ElementTree as ET
-
-# android命名空间约束
-android_scheme = "http://schemas.android.com/apk/res/android"
+import argparse
 
 """
     主要作用：检查所有 XML 文件中检查是否缺少ConstraintLayout 属性
 """
-
-
-def extract_all_attributes(xml_path, all_attributes_set):
-    ET.register_namespace('android', android_scheme)
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-
-    for elem in root.iter():
-        for attr in elem.attrib:
-            if attr.__contains__('}'):
-                newAttr = attr[attr.index('}') + 1:]
-                all_attributes_set.add(newAttr)
-
-
-def tranverseFolder(from_dir, all_attributes_set):
-    listdir = os.listdir(from_dir)
-    for fname in listdir:
-        fpath = os.path.join(from_dir, fname)
-        if os.path.isdir(fpath):
-            tranverseFolder(fpath, all_attributes_set)
-        elif os.path.isfile(fpath):
-            if fname.endswith(".xml"):
-                extract_all_attributes(fpath, all_attributes_set)
-
-
-def check_all_files(xml_directory, key_attributes):
-    all_attributes_set = set()
-    missing_attributes_set = set()
-    tranverseFolder(xml_directory, all_attributes_set)
-    # 检查是否包含关键属性
-    missing_attributes = key_attributes.difference(all_attributes_set)
-    if missing_attributes:
-        missing_attributes_set.update(missing_attributes)
-    return missing_attributes_set
 
 
 def loadXmlAttr():
@@ -55,18 +18,58 @@ def loadXmlAttr():
     return key_attributes
 
 
+def secondCheck(fpath, constranintLayoutAttrs):
+    missing_attrSet = set()
+    tree = ET.parse(fpath)
+    root = tree.getroot()
+    allAttrs = set()
+    for child in root:
+        attrib = child.attrib
+        attrName = attrib.get("name")
+        if attrName is not None and not attrName.startswith("APKTOOL_DUMMYVAL"):
+            allAttrs.add(attrName)
+    for attr in constranintLayoutAttrs:
+        if attr not in allAttrs:
+            missing_attrSet.add(attr)
+    return missing_attrSet
+
+
+# 检查按个属性重命名错误
+def checkRepeatAttr(fpath, missAttrs):
+    repeat_attrSet = set()
+    tree = ET.parse(fpath)
+    root = tree.getroot()
+    allPublicAttrs = set()
+    for child in root:
+        attrib = child.attrib
+        attrName = attrib.get("name")
+        if attrName is not None and not attrName.startswith("APKTOOL_DUMMYVAL"):
+            allPublicAttrs.add(attrName)
+    for attr in missAttrs:
+        if attr in allPublicAttrs:
+            repeat_attrSet.add(attr)
+    return repeat_attrSet
+
+
 if __name__ == "__main__":
-    # 指定XML文件目录
-    xml_directory = "/Users/shareit/work/shareit/gbwhatsapp_2.24.19.86/DecodeCode/Whatsapp_v2.24.19.86"
-    # xml_directory = "/Users/shareit/work/shareit/gbwhatsapp_2.23.20.76/DecodeCode/Whatsapp_v2.23.20.76"
-    # xml_directory = "/Users/shareit/work/shareit/wagb/DecodeCode/WhatsApp_v2.22.22.80"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from_dir")
+    args = parser.parse_args()
+    from_dir = args.from_dir
+    # 项目目录
+    # from_dir = "/Users/shareit/work/shareit/gbwhatsapp_2.24.19.86/DecodeCode/Whatsapp_v2.24.19.86"
 
     # 检查所有XML文件缺少的关键属性
-    missing_attributes_set = check_all_files(f"{xml_directory}/res/layout", loadXmlAttr())
-
+    xml_attr = loadXmlAttr()
+    missing_attributes_set = secondCheck(f"{from_dir}/res/values/attrs.xml", xml_attr)
+    repeatAttrs = checkRepeatAttr(f"{from_dir}/res/values/public.xml", missing_attributes_set)
     if missing_attributes_set:
         print("所有XML文件缺少的关键属性:")
         for attr in missing_attributes_set:
             print(attr)
+        if len(repeatAttrs) > 0:
+            print("错误重命名属性如下:")
+            for attr in repeatAttrs:
+                print(attr)
     else:
         print("所有XML文件都包含指定的关键属性.")
